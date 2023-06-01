f19d82bcc9874b1f6d50e8a6335cc359cea98826
raque <raque@raquelcorporales@gmail.com>
2023-06-01 12:57:31 +0200

    Implementacion metodos y test

--- src/multiplicacionmatriz/Matrices.java
+++ src/multiplicacionmatriz/Matrices.java
@@ -7,8 +7,90 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.File;
 
 public class Matrices {
+	
+	public static void creacionFicheros(int[][]matriz1,int[][]matriz2,int filas,int columnas) {
+		if (matriz1 == null || matriz1.length == 0 || matriz1[0] == null || matriz1[0].length == 0 || matriz2 == null || matriz2.length == 0 || matriz2[0] == null || matriz2[0].length == 0 || filas <= 0 || columnas <= 0) {
+	        throw new IllegalArgumentException("Error en formato");
+	    }
+		try {
+			FileWriter fw = new FileWriter("matriz1.txt");
+			PrintWriter pw = new PrintWriter("matriz1.txt");
+			FileWriter fw2 = new FileWriter("matriz2.txt");
+			PrintWriter pw2 = new PrintWriter("matriz2.txt");
+			for (int i = 0; i < filas; i++) {
+	            for (int j = 0; j < columnas; j++) {
+	                pw.write(matriz1[i][j] + " ");
+	                pw2.write(matriz2[i][j]+" ");
+	            }
+	            pw.println();
+	            pw2.println();
+	        }
+			pw.close();
+			pw2.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public static int[][] leerMatriz(String nombreArchivo, int filas, int columnas) throws IOException {
+			File af= new File(nombreArchivo);
+			if(af.exists()==false || filas <= 0 || columnas <= 0 ) {
+				throw new IllegalArgumentException("Error en formato o archivo no existe");
+				
+			}
+	        int[][] matriz = new int[filas][columnas];
+	        
+	        BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo));
+	        String line;
+	        int lineCount = 0;
+	        while ((line = reader.readLine()) != null) {
+	            String[] values = line.trim().split(" ");
+	            for (int j = 0; j < columnas; j++) {
+	                int value = Integer.parseInt(values[j]);
+	                matriz[lineCount][j] = value;
+	            }
+	            lineCount++;
+	        }
+	        reader.close();
+	        
+	        return matriz;
+	}
+	
+	
+	public static int[][] multiplicacionMatrices(int[][]matriz1,int[][]matriz2,int filas, int columnas) {
+		if (matriz1 == null || matriz1.length == 0 || matriz1[0] == null || matriz1[0].length == 0 || matriz2 == null || matriz2.length == 0 || matriz2[0] == null || matriz2[0].length == 0 || filas <= 0 || columnas <= 0) {
+	        throw new IllegalArgumentException("Error en formato");
+	    }
+		
+		int[][] nuevamatriz= new int[filas][columnas];
+		for (int i = 0; i < filas; i++) {
+		    for (int j = 0; j < columnas; j++) {
+		        for (int k = 0; k < columnas; k++) {
+		            nuevamatriz[i][j] += matriz1[i][k] * matriz2[k][j];
+		        }
+		    }
+		}
+		try {
+			FileWriter fw = new FileWriter("matrizresultado.txt");
+			PrintWriter pw = new PrintWriter("matrizresultado.txt");
+			for (int i = 0; i < filas; i++) {
+	            for (int j = 0; j < columnas; j++) {
+	                pw.write(nuevamatriz[i][j] + " ");
+	            }
+	            pw.println();
+	           
+	        }
+			pw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return nuevamatriz;
+	}
 
 	public static void main(String[] args) {
 		//Dimension filas y columnas
@@ -44,42 +126,15 @@ public class Matrices {
 		matriz2[2][0]= 3;
 		matriz2[2][1]= 2;
 		matriz2[2][2]= 1;
+		creacionFicheros(matriz1,matriz2,f,c);
 		
-		for (int i = 0; i < f; i++) {
-		    for (int j = 0; j < c; j++) {
-		        for (int k = 0; k < c; k++) {
-		            nuevamatriz[i][j] += matriz1[i][k] * matriz2[k][j];
-		        }
-		    }
-		}
-		
-		
-
-		
-		
-		//escribir matrices
 		try {
-			FileWriter fw = new FileWriter("matriz1.txt");
-			PrintWriter pw = new PrintWriter("matriz1.txt");
-			FileWriter fw2 = new FileWriter("matriz2.txt");
-			PrintWriter pw2 = new PrintWriter("matriz2.txt");
-			FileWriter fw3 = new FileWriter("matrizresultado.txt");
-			PrintWriter pw3 = new PrintWriter("matrizresultado.txt");
-			for (int i = 0; i < f; i++) {
-	            for (int j = 0; j < c; j++) {
-	                pw.write(matriz1[i][j] + " ");
-	                pw2.write(matriz2[i][j]+" ");
-	                pw3.write(nuevamatriz[i][j]+" ");
-	            }
-	            pw.println();
-	            pw2.println();
-	            pw3.println();
-	        }
-			pw.close();
-			pw2.close();
-			pw3.close();
+			int[][] matrizleer1= leerMatriz("matriz1.txt",f,c);
+			int[][] matrizleer2= leerMatriz("matriz1.txt",f,c);
+			multiplicacionMatrices(matrizleer1,matrizleer2,f,c);
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}	
 }
